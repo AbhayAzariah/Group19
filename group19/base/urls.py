8cca6a8431c693ca6ef772f822aaa7f2dc3894d3
rwpotter8 <rwpotter08@gmail.com>
2024-12-08 22:56:28 -0500

    Fix find url

--- group19/base/urls.py
+++ group19/base/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('find/', views.find, name="find"),
+    path('find/', views.find_university_view, name="find"),
     path('compare/', views.compare, name="compare"),
     path('login-register/', views.login_register, name="login_register"),
     path('profile/', views.profile, name='profile'),
@@ -15,5 +15,4 @@ urlpatterns = [
     path('rooms/<int:room_id>/delete/', views.delete_room, name='delete_room'),
     path('message/<int:message_id>/edit/', views.edit_message, name='edit_message'),
     path('message/<int:message_id>/delete/', views.delete_message, name='delete_message'),
-    path("find/", views.find_university_view, name="find_university"),
 ]
